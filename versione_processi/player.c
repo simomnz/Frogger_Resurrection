8f2a72e3c7af1819e359d07414f463a87aac5c61
simomnz <s.manunza21@studenti.unica.it>
2024-12-10 12:41:33 +0100

    modified:   versione_processi/player.c

--- versione_processi/player.c
+++ versione_processi/player.c
@@ -9,17 +9,22 @@ void movePlayer(Player *player, int sockfd) {
 
     switch (input) {
         case 'w':
+        case KEY_UP:
             player->cords.y--;
             break;
         case 's':
+        case KEY_DOWN:
             player->cords.y++;
             break;
         case 'a':
+        case KEY_LEFT:
             player->cords.x--;
             break;
         case 'd':
+        case KEY_RIGHT:
             player->cords.x++;
             break;
+            
         case ' ':
 
             //gestione delle granate
@@ -32,6 +37,5 @@ void movePlayer(Player *player, int sockfd) {
     //la funzione dovrebbe successivamente chiamare sendPlayerCords per inviare l'input al server   
 
     sendPlayerCords(sockfd, player);
-
 }
 
