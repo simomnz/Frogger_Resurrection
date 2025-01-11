d4064c211a78140851cb3f1adfac6bd1fc9b4050
simomnz <s.manunza21@studenti.unica.it>
2025-01-11 21:40:51 +0100

    deleted files

--- versione_processi/process.c
+++
@@ -1,8 +0,0 @@
-//questo è al singolare perchè "processes.c" fa cagare
-
-#include "process.h"
-
-
-void createProcess() {
-    
-}
--- versione_processi/process.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <sys/types.h>
-
-void createProcess();
-
-
-
